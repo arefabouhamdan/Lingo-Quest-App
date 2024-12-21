30cf8aefec51eec4c4dab52173683c20622e07b1
ArefAH <arefabouhamdan@gmail.com>
2024-12-21 16:21:51 +0200

    Tailwind <> Added color theme to match system theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,26 @@
-import { Text, View } from "react-native";
-import "../global.css"
+import React from "react";
+import { Text, useColorScheme } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import "../global.css";
 
 export default function Index() {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle = colorScheme === "light" ? "text-black" : "text-white";
+  const themeViewStyle = colorScheme === "light" ? "bg-white" : "bg-darkgray";
+  const statusBarStyle = colorScheme === "light" ? "dark" : "light";
+  const statusBarBackgroundColor =
+    colorScheme === "light" ? "#ffffff" : "#313131";
+
   return (
-    <SafeAreaView className="bg-slate-500">
-      <Text>Hello</Text>
+    <SafeAreaView
+      className={`${themeViewStyle} flex-1 justify-center items-center`}
+    >
+      <StatusBar
+        style={statusBarStyle}
+        backgroundColor={statusBarBackgroundColor}
+      />
     </SafeAreaView>
   );
 }
