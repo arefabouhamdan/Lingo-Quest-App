83ee0ea6a3815e6ce1da435d52ec405bd7358279
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-23 12:40:10 +0200

    TypeScript <> Refactored theme handling

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,21 @@
 import { Text, useColorScheme, Image, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
-import Button from "../assets/components/Button";
 import tw from "twrnc";
+import { useTheme } from "../assets/utils/useTheme";
+import Button from "../assets/components/Button";
 
 export default function Index() {
-  const colorScheme = useColorScheme();
-  const themeTextStyle = colorScheme === "light" ? "text-black" : "text-white";
-  const themeViewStyle = colorScheme === "light" ? "bg-white" : "bg-gray-800";
-  const statusBarStyle = colorScheme === "light" ? "dark" : "light";
-  const statusBarBackgroundColor =
-    colorScheme === "light" ? "#ffffff" : "#1F2937";
+  const { themeTextStyle, themeViewStyle, statusBarStyle, statusBarBackgroundColor } = useTheme();
 
   return (
     <SafeAreaView
       style={tw`${themeViewStyle} flex-1 items-center justify-center`}
     >
       <StatusBar
-        style={statusBarStyle}
         backgroundColor={statusBarBackgroundColor}
       />
-      <View style={tw`flex flex-col items-center my-auto gap-20`}>
-        <Image
-          source={require('../assets/images/game/guide-hello.png')}
-          style={tw`w-48 h-60 mb-5`}
-        />
-        <View>
-          <Button text="Login"/>
-          <Button image text="Login with Google"/>
-        </View>
-      </View>
-      <Text style={tw`${themeTextStyle} my-auto`}>
-        Don't have an account? <Text style={tw`text-sky-400 font-bold`}>Sign up</Text>
-      </Text>
+      
     </SafeAreaView>
   );
 }
--- assets/components/Button.tsx
+++ assets/components/Button.tsx
@@ -5,7 +5,7 @@ import tw from 'twrnc';
 const Button = ({style = "", image = false, text = '' }) => {
 
   return (
-    <TouchableOpacity style={tw`${style} bg-sky-400 rounded-2 w-60 h-11 mt-5 flex flex-row justify-center items-center gap-2`}>
+    <TouchableOpacity style={tw`${style} bg-sky-400 rounded-2 w-60 h-11 mt-5 flex flex-row justify-center items-center gap-5`}>
       {image && (
         <Image
           style={tw`w-7 h-7`}
