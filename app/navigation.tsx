63a39968b9a49658dc5727a564b20a50f6a3d34f
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-23 15:18:29 +0200

    React Native <> Refactored navigation to use bottom tab navigator

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Welcome from './auth/welcome';
 import Login from './auth/login';
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={Welcome} />
-      <Stack.Screen name="Login" component={Login} />
-    </Stack.Navigator>
+  return (  
+    <Tab.Navigator>
+      <Tab.Screen name="Welcome" component={Welcome} />
+      <Tab.Screen name="Login" component={Login} />
+    </Tab.Navigator>
   );
 };
 
