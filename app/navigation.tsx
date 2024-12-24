7401f4f72c34a816c7ae28c99dbd93edfaa04aab
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-24 22:34:36 +0200

    React Native <> Added Icons and styled navbar

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
 import Welcome from "./auth/welcome";
 import Login from "./auth/login";
 import Choose from "./auth/choose";
 import Student from "./auth/student";
 import Tutor from "./auth/tutor";
-import Home from "./student/home"
-import Levels from "./student/levels"
+import Home from "./student/home";
+import Levels from "./student/levels";
 import Leaderboard from "./student/leaderboard";
 import Profile from "./student/profile";
 
@@ -18,39 +19,77 @@ const loggedIn = true;
 const Navigation = () => {
   return (
     <>
-    {loggedIn ? (
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Levels" component={Levels} />
-        <Tab.Screen name="Leaderboard" component={Leaderboard} />
-        <Tab.Screen name="Profile" component={Profile} />
-      </Tab.Navigator>
-    ):(
-
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-        />
-        <Stack.Screen
-          name="Choose"
-          component={Choose}
-        />
-        <Stack.Screen
-          name="Student"
-          component={Student}
-        />
-        <Stack.Screen
-          name="Tutor"
-          component={Tutor}
-        />
-      </Stack.Navigator>
-    )}
-      </>
+      {loggedIn ? (
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+              backgroundColor: "#4FC0E8",
+              borderBottomWidth: 5,
+              borderColor: "#3AADD9",
+              height: 60,
+            },
+            tabBarLabelStyle: {
+              color: "#4FC0E8",
+            },
+            tabBarShowLabel: false,
+            tabBarIconStyle: {
+              width: 50,
+              height: 50,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 50,
+            },
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: () => (
+                <Icon name="home-outline" color={"white"} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Levels"
+            component={Levels}
+            options={{
+              tabBarIcon: () => (
+                <Icon name="bonfire-outline" color={"white"} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Leaderboard"
+            component={Leaderboard}
+            options={{
+              tabBarIcon: () => (
+                <Icon name="trophy-outline" color={"white"} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              tabBarIcon: () => (
+                <Icon name="person-outline" color={"white"} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Choose" component={Choose} />
+          <Stack.Screen name="Student" component={Student} />
+          <Stack.Screen name="Tutor" component={Tutor} />
+        </Stack.Navigator>
+      )}
+    </>
   );
 };
 
