e237456cd8d93b2bf0700a9214de3c8157cc97dd
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-27 01:07:39 +0200

    React Native <> Refactored Button component to use TypeScript for props definition and updated navigation press type

--- assets/components/Button.tsx
+++ assets/components/Button.tsx
@@ -3,13 +3,21 @@ import { TouchableOpacity, Text, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import tw from 'twrnc';
 
-const Button = ({style = "", image = false, text = '', press = '', type = '' }) => {
+type ButtonProps = {
+  style: string;
+  image: boolean;
+  text: string;
+  press: string;
+  type: string;
+};
+
+const Button = ({style, image, text, press, type } : ButtonProps) => {
   const navigation = useNavigation();
   const loginStyle = "w-60";
   const submitStyle = "w-42"
 
   return (
-    <TouchableOpacity style={tw`${style} ${type == 'submit'? submitStyle : loginStyle} bg-sky-400 rounded-2 h-11 mt-5 flex flex-row justify-center items-center gap-5`}  onPress={() => navigation.navigate(press)}>
+    <TouchableOpacity style={tw`${style} ${type == 'submit'? submitStyle : loginStyle} bg-sky-400 rounded-2 h-11 mt-5 flex flex-row justify-center items-center gap-5`}  onPress={() => navigation.navigate(press as never)}>
       {image && (
         <Image
           style={tw`w-7 h-7`}
