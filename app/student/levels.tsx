2fc013730ba491b9cca1b6adf7c1d6631e4f3813
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-27 00:57:37 +0200

    React Native <> Refactored Levels component to use FlatList for rendering levels and updated Level component to accept level prop for dynamic display

--- app/student/levels.tsx
+++ app/student/levels.tsx
@@ -1,16 +1,47 @@
 import React from "react";
-import { Text, SafeAreaView } from "react-native";
+import { SafeAreaView, FlatList } from "react-native";
 import { useTheme } from "@/assets/utils/useTheme";
 import Level from "@/assets/components/level";
 import tw from "twrnc";
 
 const Levels = () => {
-  const { themeViewStyle, themeTextStyle } = useTheme();
+  const { themeViewStyle } = useTheme();
+  const levelsData = [
+    { level: "1", status: "active" },
+    { level: "2", status: "active" },
+    { level: "3", status: "active" },
+    { level: "4", status: "inactive" },
+    { level: "5", status: "inactive" },
+    { level: "6", status: "inactive" },
+    { level: "7", status: "inactive" },
+    { level: "8", status: "inactive" },
+    { level: "9", status: "inactive" },
+    { level: "10", status: "inactive" },
+    { level: "11", status: "inactive" },
+    { level: "12", status: "inactive" },
+    { level: "13", status: "inactive" },
+    { level: "14", status: "inactive" },
+    { level: "15", status: "inactive" },
+    { level: "16", status: "inactive" },
+    { level: "17", status: "inactive" },
+    { level: "18", status: "inactive" },
+    { level: "19", status: "inactive" },
+    { level: "20", status: "inactive" },
+  ];
+
   return (
     <SafeAreaView
       style={tw`${themeViewStyle} flex-1 items-center justify-center`}
     >
-     <Level status={'active'}/> 
+      <FlatList
+        data={levelsData}
+        renderItem={({ item }) => (
+          <Level
+            status={item.status}
+            level={item.level}
+          />
+        )}
+      />
     </SafeAreaView>
   );
 };
--- assets/components/level.tsx
+++ assets/components/level.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { TouchableOpacity, Text, View } from "react-native";
-import tw from 'twrnc'
+import tw from "twrnc";
 
-const Level = ({status =''}) => {
-    const backgroundColor = status == 'active' ? 'bg-sky-400' : 'bg-gray-300'
+type LevelProps = {
+  status?: string;
+  level?: string;
+};
+
+const Level = ({ status, level }: LevelProps) => {
+  const backgroundColor = status == "active" ? "bg-sky-400" : "bg-gray-300";
+
   return (
-    <TouchableOpacity>
-      <View style={tw`${backgroundColor} w-20 h-20 rounded-full items-center justify-center`}>
-          <Text style={tw`text-white font-bold text-3xl`}>1</Text>
+    <TouchableOpacity
+      disabled={status == 'inactive'}
+      style={tw`items-center justify-center mt-10`}
+    >
+      <View
+        style={tw`${backgroundColor} w-20 h-20 rounded-full items-center justify-center`}
+      >
+        <Text style={tw`text-white font-bold text-3xl`}>{level}</Text>
       </View>
     </TouchableOpacity>
   );
