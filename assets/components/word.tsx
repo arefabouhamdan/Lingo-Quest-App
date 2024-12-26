1ab190272bd1ce70ee332a9dd8dc9a399b51af6a
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-26 17:29:32 +0200

    React Native <> Updated Word component to display translation alongside the word

--- assets/components/word.tsx
+++ assets/components/word.tsx
@@ -3,14 +3,14 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { useTheme } from "../utils/useTheme";
 import tw from "twrnc";
 
-const Word = ({ word = "" }) => {
+const Word = ({ word = "" , translation = ""}) => {
   const { themeViewStyle, themeTextStyle } = useTheme();
 
   return (
     <TouchableOpacity
       style={tw`${themeViewStyle} items-center justify-center mt-10 rounded-2 w-11/12 h-22 border-2 border-sky-400`}
     >
-      <Text style={tw`${themeTextStyle} text-xl font-bold`}>{word}</Text>
+      <Text style={tw`${themeTextStyle} text-xl font-bold`}>{word} = {translation}</Text>
     </TouchableOpacity>
   );
 };
