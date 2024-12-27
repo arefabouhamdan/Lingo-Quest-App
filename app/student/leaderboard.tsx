ba80edb9dd4ebc4d01ba0fbe07cb2b6c71d94a8b
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-27 19:15:59 +0200

    React Native <> Added leaderboard component with dynamic rank display

--- app/student/leaderboard.tsx
+++ app/student/leaderboard.tsx
@@ -1,15 +1,38 @@
 import React from "react";
-import { Text,SafeAreaView} from "react-native";
+import { Text, SafeAreaView, View, Image } from "react-native";
 import { useTheme } from "@/assets/utils/useTheme";
+import { useColorScheme } from "react-native";
 import tw from "twrnc";
 
 const Leaderboard = () => {
+  const colorScheme = useColorScheme();
   const { themeViewStyle, themeTextStyle } = useTheme();
+  const rank = "Silver";
+
   return (
     <SafeAreaView
-      style={tw`${themeViewStyle} flex-1 items-center justify-center`}
+      style={tw`${themeViewStyle} flex-1 items-center justify-start`}
     >
-      <Text>Leader Board</Text>
+      <View
+        style={tw`${themeViewStyle} flex flex-row items-center p-5 justify-center gap-5 mt-10 rounded-lg w-11/12 h-22 border border-gray-200`}
+      >
+        {colorScheme === "light" ? (
+          <Image
+            source={require("@/assets/images/icons/shield-black.png")}
+            style={tw``}
+          />
+        ) : (
+          <Image
+            source={require("@/assets/images/icons/shield-white.png")}
+            style={tw``}
+          />
+        )}
+        <Text style={tw`${themeTextStyle} text-2xl font-bold `}>
+          {rank} League
+        </Text>
+      </View>
+      
+      
     </SafeAreaView>
   );
 };
