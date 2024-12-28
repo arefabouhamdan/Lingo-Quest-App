1955ecef6e1b99a4143b87b9c01ad244d633f471
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-28 13:12:37 +0200

    React Native <> Refactored leaderList component to display user information with a new User component

---
+++ assets/components/User.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { useTheme } from "@/assets/utils/useTheme";
+import tw from "twrnc";
+
+type UserProps = {
+  name: string;
+  info: string;
+  avatar: string;
+};
+
+const User = ({ name, info, avatar }: UserProps) => {
+  const { themeTextStyle } = useTheme();
+
+  return (
+    <View style={tw`flex flex-row items-center gap-5 p-5 rounded border border-gray-200 mb-5`}>
+      <Image
+        source={require(`@/assets/images/character/Aref.png`)}
+        style={tw`w-9 h-14`}
+      />
+      <View>
+          <Text style={tw`${themeTextStyle} text-xl`}>{name}</Text>
+          <Text style={tw`${themeTextStyle} text-lg`}>{info}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default User;
--- assets/components/leaderList.tsx
+++ assets/components/leaderList.tsx
@@ -1,36 +1,31 @@
 import React from "react";
 import { FlatList, Text, View } from "react-native";
+import User from "./User";
 import tw from "twrnc";
 
 const LeaderList = () => {
   const data = [
-    { username: "Aref", key: "1" },
-    { username: "Oday", key: "2" },
-    { username: "Rony", key: "3" },
-    { username: "Majd", key: "4" },
-    { username: "Aya", key: "5" },
-    { username: "Sara", key: "6" },
-    { username: "Lina", key: "7" },
-    { username: "Samer", key: "8" },
-    { username: "Layan", key: "9" },
-    { username: "Lara", key: "10" },
-    { username: "Layan", key: "11" },
-    { username: "Lara", key: "12" },
-    { username: "Layan", key: "13" },
-    { username: "Lara", key: "14" },
-    { username: "Layan", key: "15" },
-    { username: "Lara", key: "16" },
-    { username: "Layan", key: "17" },
-    { username: "Lara", key: "18" },
-    { username: "Jihad", key: "19" },
+    { name: "Aref", info: "12000", key: "1" },
+    { name: "Ali", info: "10000", key: "2" },
+    { name: "Mehdi", info: "9000", key: "3" },
+    { name: "Amir", info: "8000", key: "4" },
+    { name: "Reza", info: "7000", key: "5" },
+    { name: "Hossein", info: "6000", key: "6" },
+    { name: "Mohammad", info: "5000", key: "7" },
+    { name: "Sina", info: "4000", key: "8" },
+    { name: "Parsa", info: "3000", key: "9" },
+    { name: "Mina", info: "2000", key: "10" },
   ];
+
   return (
     <View style={tw`w-11/12 flex-1 border border-gray-200 rounded-lg `}>
-        <FlatList
-          data={data}
-          renderItem={({ item }) => <Text style={tw`text-xl`}>{item.username}</Text>}
-          style={tw`p-5 `}
-        />
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <User name={item.name} avatar={item.name} info={item.info} />
+        )}
+        style={tw`p-5 `}
+      />
     </View>
   );
 };
