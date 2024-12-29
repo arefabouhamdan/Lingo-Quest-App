52f7b372897dfa7b2eccdcb9b20a868144d9a751
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-29 16:03:04 +0200

    Tailwind <> Updated User component layout and change icon direction

--- assets/components/User.tsx
+++ assets/components/User.tsx
@@ -17,21 +17,22 @@ const User = ({ name, info, avatar }: UserProps) => {
 
   return (
     <TouchableOpacity
-      style={tw`flex flex-row items-center gap-5 p-4 rounded border-2 border-gray-200 mb-4 h-20`}
+      style={tw`flex flex-row items-center gap-5 p-4 rounded border-2 border-gray-200 mb-4 h-20 justify-between`}
     >
-      <Image
-        source={require("@/assets/images/character/Aref.png")}
-        style={tw`w-7 h-10`}
-      />
-      <View>
-        <Text style={tw`${themeTextStyle} text-xl font-bold`}>{name}</Text>
-        <Text style={tw`${themeTextStyle} text-lg font-medium`}>{info}</Text>
+      <View style={tw`flex flex-row items-center gap-5`}>
+        <Image
+          source={require("@/assets/images/character/Aref.png")}
+          style={tw`w-7 h-10`}
+        />
+        <View>
+          <Text style={tw`${themeTextStyle} text-xl font-bold`}>{name}</Text>
+          <Text style={tw`${themeTextStyle} text-lg font-medium`}>{info}</Text>
+        </View>
       </View>
       <Icon
-        name="chevron-back-outline"
+        name="chevron-forward-outline"
         color={colorScheme === "light" ? "#000" : "#fff"}
         size={26}
-        style={tw`justify-self-start`}
       />
     </TouchableOpacity>
   );
