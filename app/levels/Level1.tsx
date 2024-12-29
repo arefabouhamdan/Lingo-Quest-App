90f2261a7f9da604ca8a0a39401ed35b3c6e7eec
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-30 00:51:39 +0200

    React Native <> Added Level 1 elements

--- app/levels/Level1.tsx
+++ app/levels/Level1.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { SafeAreaView, TouchableOpacity, Text } from "react-native";
+import { SafeAreaView, Image, TouchableOpacity, Text } from "react-native";
 import { useTheme } from "@/assets/utils/useTheme";
-import Back from '@/assets/components/Back'
+import Back from "@/assets/components/Back";
 import tw from "twrnc";
 
 const Level1 = () => {
@@ -11,8 +11,9 @@ const Level1 = () => {
     <SafeAreaView
       style={tw`${themeViewStyle} flex-1 items-center justify-center`}
     >
-      <Back text="Level 1"/>
-    
+      <Back text="Level 1" />
+      <Image source={require('@/assets/images/game/Barista.png')}/>
+      <Image source={require('@/assets/images/game/coffeeshop-night-1.png')}/>
     </SafeAreaView>
   );
 };
