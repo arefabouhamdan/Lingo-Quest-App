a30dfede3b1c6db0180fa6e7c386a12a1bb3a261
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-29 18:10:06 +0200

    React Native <> Refactored Button component to use Icon instead of Image

--- assets/components/Button.tsx
+++ assets/components/Button.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { TouchableOpacity, Text, Image } from "react-native";
+import { TouchableOpacity, Text, useColorScheme } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
 import tw from "twrnc";
 
 type ButtonProps = {
-  image: boolean;
+  image: string;
   text: string;
   press: string;
   type: string;
 };
 
 const Button = ({ image, text, press, type }: ButtonProps) => {
+  const colorScheme = useColorScheme();
   const navigation = useNavigation();
   const loginStyle = "w-60";
   const submitStyle = "w-42";
@@ -23,9 +25,10 @@ const Button = ({ image, text, press, type }: ButtonProps) => {
       onPress={() => navigation.navigate(press as never)}
     >
       {image && (
-        <Image
-          style={tw`w-7 h-7`}
-          source={require("../images/icons/google.png")}
+        <Icon
+          name={image}
+          color={colorScheme === "light" ? "#000" : "#fff"}
+          size={26}
         />
       )}
       <Text style={tw`text-white font-bold text-lg`}>{text}</Text>
