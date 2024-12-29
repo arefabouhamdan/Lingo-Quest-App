fb0fc590daa0e76c5ad236c2f0115bc7dbaab551
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-29 16:49:51 +0200

    React Native <> Added user avatar and info to profile screen

--- app/student/profile.tsx
+++ app/student/profile.tsx
@@ -1,15 +1,36 @@
 import React from "react";
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, Image, View } from "react-native";
 import { useTheme } from "@/assets/utils/useTheme";
 import tw from "twrnc";
 
 const Profile = () => {
   const { themeViewStyle, themeTextStyle } = useTheme();
+  const user = {
+    name: "Aref",
+    id: "1234",
+    joined: "2021-09-01",
+  };
+  const friends = ["Aref", "Bilal", "Hassan", "Ali"];
   return (
     <SafeAreaView
-      style={tw`${themeViewStyle} flex-1 items-center justify-center`}
+      style={tw`${themeViewStyle} flex-1`}
     >
-      <Text>Profile</Text>
+      <View>
+        <Image source={require('@/assets/images/character/Aref.png')}/>
+      </View>
+      <Text>{user.name}</Text>
+      <View>
+        <View>
+          <Text>{user.joined}</Text>
+          <Text>ID {user.id}</Text>
+        </View>
+        <View>
+          <Text>Language <Image source={require('@/assets/images/flags/German.png')}/></Text>
+          <Text>Friends {friends.length}</Text>
+        </View>
+      </View>
+
+
     </SafeAreaView>
   );
 };
