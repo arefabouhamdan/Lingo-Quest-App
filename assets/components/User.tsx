aa914b62f2a5fb3bb866db959364f41c0a23c350
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-29 16:02:31 +0200

    React Native <> Added arrow next to user info

--- assets/components/User.tsx
+++ assets/components/User.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { useTheme } from "@/assets/utils/useTheme";
+import Icon from "react-native-vector-icons/Ionicons";
+import { useColorScheme } from "react-native";
 import tw from "twrnc";
 
 type UserProps = {
@@ -10,17 +12,27 @@ type UserProps = {
 };
 
 const User = ({ name, info, avatar }: UserProps) => {
+  const colorScheme = useColorScheme();
   const { themeTextStyle } = useTheme();
 
   return (
     <TouchableOpacity
       style={tw`flex flex-row items-center gap-5 p-4 rounded border-2 border-gray-200 mb-4 h-20`}
     >
-      <Image source={require('@/assets/images/character/Aref.png')} style={tw`w-7 h-10`} />
+      <Image
+        source={require("@/assets/images/character/Aref.png")}
+        style={tw`w-7 h-10`}
+      />
       <View>
         <Text style={tw`${themeTextStyle} text-xl font-bold`}>{name}</Text>
         <Text style={tw`${themeTextStyle} text-lg font-medium`}>{info}</Text>
       </View>
+      <Icon
+        name="chevron-back-outline"
+        color={colorScheme === "light" ? "#000" : "#fff"}
+        size={26}
+        style={tw`justify-self-start`}
+      />
     </TouchableOpacity>
   );
 };
