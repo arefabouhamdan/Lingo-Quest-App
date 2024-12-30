b89311e159b9a36a8a4fd2e30e6d3cb9a34153cd
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-30 11:25:46 +0200

    React Native <> Added push-to-talk button with icon to Level 1 screen

--- app/levels/Level1.tsx
+++ app/levels/Level1.tsx
@@ -5,13 +5,16 @@ import {
   View,
   TouchableOpacity,
   Text,
+  useColorScheme
 } from "react-native";
 import { useTheme } from "@/assets/utils/useTheme";
 import Back from "@/assets/components/Back";
+import Icon from "react-native-vector-icons/Ionicons";
 import tw from "twrnc";
 
 const Level1 = () => {
   const { themeViewStyle } = useTheme();
+  const colorScheme = useColorScheme();
 
   return (
     <SafeAreaView style={tw`${themeViewStyle}flex-1 items-center`}>
@@ -26,6 +29,14 @@ const Level1 = () => {
           style={tw`flex-1 w-55 h-100 absolute bottom-0`}
         />
       </View>
+      <TouchableOpacity>
+        <Icon
+          name='mic'
+          color={colorScheme === "light" ? "#000" : "#fff"}
+          size={26}
+        />
+        <Text>Push to talk</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
