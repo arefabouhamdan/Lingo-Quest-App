c2974cb806a7b4eaede80bef81ee218bb0dc2a61
arefabouhamdan <arefabouhamdan@gmail.com>
2024-12-31 01:04:17 +0200

    React Native <> Added languageBar and Tests counter to TutorHome screen

--- app/tutor/TutorHome.tsx
+++ app/tutor/TutorHome.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import LanguageBar from "@/assets/components/languageBar";
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+import { useTheme } from "@/assets/utils/useTheme";
+import tw from "twrnc";
 
 const TutorHome = () => {
-  return (
-    <View><Text>Tutor</Text></View>
-  )
-}
+  const { themeViewStyle, themeTextStyle } = useTheme();
+  const tests = 10;
 
-export default TutorHome
+  const borderStyle = `${themeViewStyle} flex items-center p-5 justify-center gap-5 mt-10 rounded-lg w-11/12 h-30 border border-gray-200`;
+  return (
+    <SafeAreaView style={tw`${themeViewStyle} flex-1 items-center`}>
+      <LanguageBar />
+      <View style={tw`${borderStyle}`}>
+        <Text style={tw`${themeTextStyle} text-xl font-bold`}>Number of Tests</Text>
+        <Text style={tw`${themeTextStyle} text-3xl font-bold`}>{tests}</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default TutorHome;
