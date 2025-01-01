ebdae84bb0659b9696207459aaf9c4fbaeb01ef3
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-01 18:04:53 +0200

    React Native <> Implemented dynamic test data display for selected student in Test component

--- app/tutor/Test.tsx
+++ app/tutor/Test.tsx
@@ -10,13 +10,95 @@ type TestProps = {
   };
 };
 
-const Test = ({ route } : TestProps) => {
+const Test = ({ route }: TestProps) => {
   const { userName } = route.params;
+
+  const tests = [
+    {
+      student: "Aref",
+      testData: [
+        {
+          response_type: "voice",
+          question: "What is the capital of France?",
+          answer: "Paris",
+        },
+        {
+          response_type: "text",
+          question: "What is the capital of Germany?",
+          answer: "Berlin",
+        },
+        {
+          response_type: "voice",
+          question: "What is the capital of Italy?",
+          answer: "Rome",
+        },
+        {
+          response_type: "text",
+          question: "What is the capital of Spain?",
+          answer: "Madrid",
+        }
+      ],
+    },
+    {
+      student: "Oday",
+      testData: [
+        {
+          response_type: "voice",
+          question: "What is the capital of France?",
+          answer: "Lyon",
+        },
+        {
+          response_type: "text",
+          question: "What is the capital of Germany?",
+          answer: "Munich",
+        },
+        {
+          response_type: "voice",
+          question: "What is the capital of Italy?",
+          answer: "Rome",
+        },
+        {
+          response_type: "text",
+          question: "What is the capital of Spain?",
+          answer: "Madrid",
+        }
+      ],
+    },
+    {
+      student: "Rony",
+      testData: [
+        {
+          response_type: "voice",
+          question: "What is the capital of France?",
+          answer: "Nice",
+        },
+        {
+          response_type: "text",
+          question: "What is the capital of Germany?",
+          answer: "Dusseldorf",
+        },
+        {
+          response_type: "voice",
+          question: "What is the capital of Italy?",
+          answer: "Rome",
+        },
+        {
+          response_type: "text",
+          question: "What is the capital of Spain?",
+          answer: "Madrid",
+        }
+      ],
+    }
+  ];
+
+  const studentData = tests.find((test) => test.student === userName);
 
   return (
     <SafeAreaView>
       <View>
-        <Text>{userName}</Text>
+        <Text>{studentData?.student}</Text>
+        <Text>{studentData?.testData[0]?.question}</Text>
+        <Text>{studentData?.testData[0]?.answer}</Text>
       </View>
     </SafeAreaView>
   );
