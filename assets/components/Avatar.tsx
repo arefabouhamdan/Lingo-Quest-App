12103c7aa8cf1ea2675dc7b2f58086076e9d532e
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-05 18:32:49 +0200

    React Native <> Refactored Avatar component to change between male and female avatars

--- assets/components/Avatar.tsx
+++ assets/components/Avatar.tsx
@@ -7,11 +7,16 @@ import MaleFace from "../images/character/maleFace";
 import Eyes from "../images/character/Eyes";
 import ManShirt from "../images/character/manShirt";
 import tw from "twrnc";
+import WomanShirt from "../images/character/womanShirt";
+import WomanFace from "../images/character/womanFace";
+import WomanHairTwo from "../images/character/womanHairTwo";
+import WomanHairThree from "../images/character/womanHairThree";
 
 const Avatar = () => {
   const user = {
     type: "student",
     avatar: {
+      gender: 'female',
       hair: { color: "#674238", style: "ManHairOne" },
       skin: { color: "#E7BC98", style: "MaleFace" },
       eyes: { color: "#1E81C8", style: "Eyes" },
@@ -20,12 +25,21 @@ const Avatar = () => {
   };
 
   return (
+    user.avatar.gender == 'male' ? (
     <View style={tw`flex flex-col items-center justify-end absolute`}>
-      <ManHairThree hairColor={user.avatar.hair.color} style="relative top-17"/>
+      <ManHairOne hairColor={user.avatar.hair.color} style="relative top-17"/>
       <Eyes eyeColor={user.avatar.eyes.color} style="relative top-14.5"/>
       <MaleFace skinColor={user.avatar.skin.color} />
       <ManShirt shirtColor={user.avatar.shirt.color} />
     </View>
+    ) : (
+      <View style={tw`flex flex-col items-center justify-end absolute`}>
+        <WomanHairThree hairColor={user.avatar.hair.color} style="relative top-40.8"/>
+        <Eyes eyeColor={user.avatar.eyes.color} style="relative top-11"/>
+        <WomanFace skinColor={user.avatar.skin.color} />
+        <WomanShirt shirtColor={user.avatar.shirt.color} />
+      </View>
+    )
   );
 };
 
