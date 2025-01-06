dfcdcfe151d6eab74b29a518fb613550d0bffa79
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-06 18:14:25 +0200

    React Native <> Refactored Avatar to accept user prop and background color

--- app/auth/createAvatar.tsx
+++ app/auth/createAvatar.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import { SafeAreaView, View, TouchableOpacity, Text } from "react-native";
 import { useTheme } from "@/assets/utils/useTheme";
 import tw from "twrnc";
+import Avatar from "@/assets/components/Avatar";
 
 const CreateAvatar = () => {
   const { themeViewStyle } = useTheme();
   const buttonStyle = 'flex justify-center items-center p-1 w-9/20 h-14 rounded-2 border-r-4 border-b-4';
   const fontStyle = 'text-white font-bold text-lg';
+  const user = {
+    type: "student",
+    avatar: {
+      gender: "male",
+      background: { color: "green"},
+      hair: { color: "#674238", style: "ManHairOne" },
+      skin: { color: "#E7BC98", style: "MaleFace" },
+      eyes: { color: "#1E81C8", style: "Eyes" },
+      shirt: { color: "#484848", style: "ManShirt" },
+    },
+  };
   return (
     <SafeAreaView style={tw`${themeViewStyle} flex-1`}>
       <View style={tw`flex flex-row justify-between items-center px-5 mt-5`}>
@@ -25,6 +37,7 @@ const CreateAvatar = () => {
           </Text>
         </TouchableOpacity>
       </View>
+      <Avatar user={user}/>
     </SafeAreaView>
   );
 };
--- assets/components/Avatar.tsx
+++ assets/components/Avatar.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { View } from 'react-native';
-import tw from 'twrnc';
-import ManHairOne from '../images/character/manHairOne';
-import ManHairTwo from '../images/character/manHairTwo';
-import ManHairThree from '../images/character/manHairThree';
-import WomanHairTwo from '../images/character/womanHairTwo';
-import WomanHairThree from '../images/character/womanHairThree';
-import MaleFace from '../images/character/maleFace';
-import WomanFace from '../images/character/womanFace';
-import ManShirt from '../images/character/manShirt';
-import WomanShirt from '../images/character/womanShirt';
-import Eyes from '../images/character/Eyes';
-import MaleEyes from '../images/character/maleEyes';
-import FemaleEyes from '../images/character/femaleEyes';
+import React from "react";
+import { View } from "react-native";
+import tw from "twrnc";
+import ManHairOne from "../images/character/manHairOne";
+import ManHairTwo from "../images/character/manHairTwo";
+import ManHairThree from "../images/character/manHairThree";
+import WomanHairTwo from "../images/character/womanHairTwo";
+import WomanHairThree from "../images/character/womanHairThree";
+import MaleFace from "../images/character/maleFace";
+import WomanFace from "../images/character/womanFace";
+import ManShirt from "../images/character/manShirt";
+import WomanShirt from "../images/character/womanShirt";
+import Eyes from "../images/character/Eyes";
+import MaleEyes from "../images/character/maleEyes";
+import FemaleEyes from "../images/character/femaleEyes";
 
-const Avatar = () => {
+type AvatarProps = {
+  user: {
+    type: string;
+    avatar: {
+      gender: string;
+      background: { color: string };
+      hair: { color: string; style: string };
+      skin: { color: string; style: string };
+      eyes: { color: string; style: string };
+      shirt: { color: string; style: string };
+    };
+  };
+};
+
+const Avatar = ({ user }: AvatarProps) => {
   const avatarComponents = {
     hair: {
       ManHairOne,
@@ -34,23 +48,12 @@ const Avatar = () => {
     eyes: {
       Eyes,
       MaleEyes,
-      FemaleEyes
-    },
-  };
-
-  const user = {
-    type: "student",
-    avatar: {
-      gender: 'male',
-      hair: { color: "#674238", style: "ManHairOne" },
-      skin: { color: "#E7BC98", style: "MaleFace" },
-      eyes: { color: "#1E81C8", style: "Eyes" },
-      shirt: { color: "#484848", style: "ManShirt" },
+      FemaleEyes,
     },
   };
 
   const { avatar } = user;
-  const { hair, skin, eyes, shirt } = avatar;
+  const { hair, skin, eyes, shirt, background } = avatar;
 
   const HairComponent = avatarComponents.hair[hair.style];
   const FaceComponent = avatarComponents.face[skin.style];
@@ -58,9 +61,9 @@ const Avatar = () => {
   const EyesComponent = avatarComponents.eyes[eyes.style];
 
   return (
-    <View style={tw`flex flex-col items-center justify-end absolute`}>
-      <HairComponent hairColor={hair.color} style="relative top-17 z-2"/>
-      <EyesComponent eyeColor={eyes.color} style="relative top-14.5 z-1"/>
+    <View style={[tw`flex flex-col items-center justify-end absolute`, { backgroundColor: background.color }]}>
+      <HairComponent hairColor={hair.color} style="relative top-17 z-2" />
+      <EyesComponent eyeColor={eyes.color} style="relative top-14.5 z-1" />
       <FaceComponent skinColor={skin.color} />
       <ShirtComponent shirtColor={shirt.color} />
     </View>
