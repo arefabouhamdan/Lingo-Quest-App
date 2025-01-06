f4293a0aff54701906f97df25d4be726ded8c289
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-06 17:06:25 +0200

    React Native <> Refactored Avatar component to use the component that matches the style found in the user object

--- assets/components/Avatar.tsx
+++ assets/components/Avatar.tsx
@@ -1,18 +1,41 @@
-import React from "react";
-import { View } from "react-native";
-import ManHairOne from "../images/character/manHairOne";
-import ManHairTwo from "../images/character/manHairTwo";
-import ManHairThree from "../images/character/manHairThree";
-import MaleFace from "../images/character/maleFace";
-import Eyes from "../images/character/Eyes";
-import MaleEyes from "../images/character/maleEyes";
-import FemaleEyes from "../images/character/femaleEyes";
-import ManShirt from "../images/character/manShirt";
-import tw from "twrnc";
-import WomanShirt from "../images/character/womanShirt";
-import WomanFace from "../images/character/womanFace";
-import WomanHairTwo from "../images/character/womanHairTwo";
-import WomanHairThree from "../images/character/womanHairThree";
+import React from 'react';
+import { View } from 'react-native';
+import tw from 'twrnc';
+import ManHairOne from '../images/character/manHairOne';
+import ManHairTwo from '../images/character/manHairTwo';
+import ManHairThree from '../images/character/manHairThree';
+import WomanHairTwo from '../images/character/womanHairTwo';
+import WomanHairThree from '../images/character/womanHairThree';
+import MaleFace from '../images/character/maleFace';
+import WomanFace from '../images/character/womanFace';
+import ManShirt from '../images/character/manShirt';
+import WomanShirt from '../images/character/womanShirt';
+import Eyes from '../images/character/Eyes';
+import MaleEyes from '../images/character/maleEyes';
+import FemaleEyes from '../images/character/femaleEyes';
+
+const avatarComponents = {
+  hair: {
+    ManHairOne,
+    ManHairTwo,
+    ManHairThree,
+    WomanHairTwo,
+    WomanHairThree,
+  },
+  face: {
+    MaleFace,
+    WomanFace,
+  },
+  shirt: {
+    ManShirt,
+    WomanShirt,
+  },
+  eyes: {
+    Eyes,
+    MaleEyes,
+    FemaleEyes
+  },
+};
 
 const Avatar = () => {
   const user = {
@@ -27,24 +50,20 @@ const Avatar = () => {
   };
 
   const { avatar } = user;
-  const { hair, skin, eyes, shirt, gender } = avatar;
+  const { hair, skin, eyes, shirt } = avatar;
+
+  const HairComponent = avatarComponents.hair[hair.style];
+  const FaceComponent = avatarComponents.face[skin.style];
+  const ShirtComponent = avatarComponents.shirt[shirt.style];
+  const EyesComponent = avatarComponents.eyes[eyes.style];
 
   return (
-    gender == 'male' ? (
     <View style={tw`flex flex-col items-center justify-end absolute`}>
-      <ManHairTwo hairColor={hair.color} style="relative top-17 z-2"/>
-      <MaleEyes eyeColor={eyes.color} style="relative top-14.5 z-1"/>
-      <MaleFace skinColor={skin.color} />
-      <ManShirt shirtColor={shirt.color} />
+      <HairComponent hairColor={hair.color} style="relative top-17 z-2"/>
+      <EyesComponent eyeColor={eyes.color} style="relative top-14.5 z-1"/>
+      <FaceComponent skinColor={skin.color} />
+      <ShirtComponent shirtColor={shirt.color} />
     </View>
-    ) : (
-      <View style={tw`flex flex-col items-center justify-end absolute`}>
-        <WomanHairThree hairColor={hair.color} style="relative top-41.8 right-0.4 z-2"/>
-        <FemaleEyes eyeColor={eyes.color} style="relative top-11 z-1"/>
-        <WomanFace skinColor={skin.color} />
-        <WomanShirt shirtColor={shirt.color} />
-      </View>
-    )
   );
 };
 
