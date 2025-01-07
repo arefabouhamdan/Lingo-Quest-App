f071a675bafb3ecce94db638d18b3a8522913ad5
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-07 02:38:27 +0200

    Tailwind <> Updated searchBar style

--- assets/components/Search.tsx
+++ assets/components/Search.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextInput, View } from "react-native";
+import { TextInput, View, useColorScheme } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { useTheme } from "../utils/useTheme";
 import tw from "twrnc";
@@ -10,12 +10,13 @@ type SearchProps = {
 
 const Search = ({ text } : SearchProps) => {
   const { themeViewStyle } = useTheme();
+  const colorScheme = useColorScheme();
 
   return (
     <View
-      style={tw`${themeViewStyle} flex-row items-center bg-white rounded border-b-4 border-r-4 border-sky-400 px-3 py-2`}
+      style={tw`${themeViewStyle} flex-row items-center rounded border-2 border-sky-400 px-3 py-2`}
     >
-      <Icon name="search" size={20} color="#000" style={tw`mr-2`} />
+      <Icon name="search" size={20} color={colorScheme == 'light' ? '#000' : '#fff'} style={tw`mr-2`} />
       <TextInput
         style={tw`flex-1 text-base`}
         placeholder={text}
