95353e89f4d8d8f345bc8dcf45e97321037b9f15
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-07 02:34:56 +0200

    React Native <> Added friends FlatList in the friends screen

--- app/student/friends.tsx
+++ app/student/friends.tsx
@@ -2,9 +2,11 @@ import Search from "@/assets/components/Search";
 import User from "@/assets/components/User";
 import React from "react";
 import { FlatList, SafeAreaView, View } from "react-native";
+import { useTheme } from "@/assets/utils/useTheme";
 import tw from "twrnc";
 
 const Friends = () => {
+  const { themeViewStyle } = useTheme();
   const user = {
     name: "Aref",
     id: "1234",
@@ -20,16 +22,33 @@ const Friends = () => {
       eyes: { color: "#1E81C8", style: "Eyes" },
       shirt: { color: "#484848", style: "ManShirt" },
     },
-    friends: ["Aref", "Bilal", "Hassan", "Ali"],
+    friends: [
+      { name: "Aref", dateAdded: "01-09-2021" },
+      { name: "Bilal", dateAdded: "01-09-2021" },
+      { name: "Hassan", dateAdded: "01-09-2021" },
+      { name: "Ali", dateAdded: "01-09-2021" },
+      { name: "Aref", dateAdded: "01-09-2021" },
+      { name: "Bilal", dateAdded: "01-09-2021" },
+      { name: "Hassan", dateAdded: "01-09-2021" },
+      { name: "Ali", dateAdded: "01-09-2021" },
+      { name: "Aref", dateAdded: "01-09-2021" },
+      { name: "Bilal", dateAdded: "01-09-2021" },
+      { name: "Hassan", dateAdded: "01-09-2021" },
+      { name: "Ali", dateAdded: "01-09-2021" },
+    ],
   };
   return (
-    <SafeAreaView style={tw`flex items-center`}>
+    <SafeAreaView style={tw`${themeViewStyle} flex-1 items-center`}>
       <View style={tw`mt-5 w-11/12`}>
         <Search text="Lost your friend? Search for him" />
       </View>
-      <FlatList data={user.friends} renderItem={(item) => (
-        <User name={item.item} />
-      )}/>
+      <FlatList
+        data={user.friends}
+        renderItem={({ item }) => (
+          <User name={item.name} info={`${item.dateAdded}`} />
+        )}
+        style={tw`mt-5 w-11/12`}
+      />
     </SafeAreaView>
   );
 };
