54f788c166ccfdbc2dd062b3f3e83529959b023d
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-07 17:07:57 +0200

    React Native <> Added User's info to the friendModal

--- assets/components/friendModal.tsx
+++ assets/components/friendModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Text, TouchableOpacity, View } from "react-native";
+import { Modal, Text, TouchableOpacity, View, Image } from "react-native";
 import { useTheme } from "../utils/useTheme";
 import tw from "twrnc";
 import Avatar from "./Avatar";
@@ -30,6 +30,7 @@ const user = {
 
 const FriendModal = ({ modalVisible, setModalVisible }: FriendModalProps) => {
   const { themeViewStyle, themeTextStyle } = useTheme();
+  const { name, id, joined, friends } = user;
 
   return (
     <Modal
@@ -38,11 +39,44 @@ const FriendModal = ({ modalVisible, setModalVisible }: FriendModalProps) => {
       visible={modalVisible}
       style={tw`bg-black`}
     >
-      <View style={tw`flex-1 justify-center items-center bg-black bg-opacity-50`}>
+      <View
+        style={tw`flex-1 justify-center items-center bg-black bg-opacity-50`}
+      >
         <View
-          style={tw`${themeViewStyle} border-4 border-white rounded-2 w-92 h-120 flex flex-col items-center`}
+          style={tw`${themeViewStyle} border-4 border-white rounded-2 w-92 h-130 flex flex-col items-center`}
         >
           <Avatar user={user} />
+          <View
+            style={tw`flex flex-row px-3 justify-between items-center w-full mt-5`}
+          >
+            <Text style={tw`${themeTextStyle} text-2xl font-bold text-left `}>
+              {name}
+            </Text>
+            <Image
+              style={tw`h-4.4 w-6`}
+              source={require("@/assets/images/flags/German.png")}
+            />
+          </View>
+          <View style={tw`flex flex-row justify-between items-center w-full`}>
+            <View style={tw`flex flex-col px-3`}>
+              <View
+                style={tw`flex flex-row items-center mt-5 w-full justify-between`}
+              >
+                <Text style={tw`${themeTextStyle} text-lg font-medium mt-2`}>
+                  ID {id}
+                </Text>
+                <Text style={tw`${themeTextStyle} text-lg font-medium `}>
+                  No. Friends{"  "}
+                  <Text style={tw`text-sky-400 font-extrabold`}>
+                    {friends.length}
+                  </Text>
+                </Text>
+              </View>
+              <Text style={tw`${themeTextStyle} text-lg font-medium`}>
+                Joined {joined}
+              </Text>
+            </View>
+          </View>
           <TouchableOpacity
             style={tw`absolute top-5 right-5`}
             onPress={() => setModalVisible(!modalVisible)}
@@ -50,7 +84,7 @@ const FriendModal = ({ modalVisible, setModalVisible }: FriendModalProps) => {
             <Icon name="close-outline" color="white" size={36} />
           </TouchableOpacity>
           <TouchableOpacity
-            style={tw`w-52 bg-sky-400 rounded-2 h-14 mt-5 flex flex-row justify-center items-center gap-5`}
+            style={tw`w-52 bg-sky-400 rounded-2 h-14 my-auto flex flex-row justify-center items-center gap-5`}
             onPress={() => console.log("Friend Added")}
           >
             <Icon name="add-outline" color="white" size={26} />
