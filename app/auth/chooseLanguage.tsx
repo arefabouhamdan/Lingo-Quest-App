eb4c82ecd6ccb8a2fff3bd137af0ed438d70b432
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 13:42:35 +0200

    React Native <> Created chooseLanguage screen

--- app/auth/chooseLanguage.tsx
+++ app/auth/chooseLanguage.tsx
@@ -5,7 +5,7 @@ import tw from "twrnc";
 import Button from "../../assets/components/Button";
 import Back from "@/assets/components/Back";
 
-const Choose = () => {
+const ChooseLanguage = () => {
   const { themeTextStyle, themeViewStyle } = useTheme();
 
   return (
@@ -18,16 +18,10 @@ const Choose = () => {
           source={require("../../assets/images/game/guide-choose.png")}
           style={tw`w-52 h-68 mb-5`}
         />
-        <View style={tw`flex items-center gap-2`}>
-          <Text style={tw`${themeTextStyle} my-auto text-xl font-bold`}>
-            Choose your role
-          </Text>
-          <Button text="Student" press="Student" type="submit"/>
-          <Button text="Tutor" press="Tutor" type="submit"/>
-        </View>
+        
       </View>
     </SafeAreaView>
   );
 };
 
-export default Choose;
+export default ChooseLanguage;
