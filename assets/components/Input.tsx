21418bad60eae968408da174705ec514c7f47504
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 20:10:22 +0200

    Tailwind <> Updated input component styling

--- assets/components/Input.tsx
+++ assets/components/Input.tsx
@@ -9,7 +9,7 @@ type InputProps = {
 const Input = ({placeholder} : InputProps) => {
   return (
     <TextInput
-      style={tw`text-white bg-sky-400 w-90 h-11 rounded-2 mt-2 pl-3`}
+      style={tw`text-white w-90 h-11 rounded-2 border border-gray-200 mt-2 pl-3`}
       placeholder={placeholder}
       placeholderTextColor="white"
     />
