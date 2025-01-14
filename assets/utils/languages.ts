4c15a0f46440b1c95d4740c6ec234281c0128d1a
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 14:23:50 +0200

    React Native <> Added source to language options

--- assets/utils/languages.ts
+++ assets/utils/languages.ts
@@ -3,30 +3,36 @@ export const languages = [
         id: 1,
         name: "English",
         code: "en",
+        source: require("../images/flags/English.png"),
     },
     {
         id: 2,
         name: "Spanish",
         code: "es",
+        source: require("../images/flags/Spanish.png"),
     },
     {
         id: 3,
         name: "French",
         code: "fr",
+        source: require("../images/flags/French.png"),
     },
     {
         id: 4,
         name: "German",
         code: "de",
+        source: require("../images/flags/German.png"),
     },
     {
         id: 5,
         name: "Italian",
         code: "it",
+        source: require("../images/flags/Italian.png"),
     },
     {
         id: 6,
         name: "Arabic",
         code: "ar",
+        source: require("../images/flags/Arabic.png"),
     }
 ]
