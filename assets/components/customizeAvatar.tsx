302fbac8b2a28b884a944363fb36a8e436e8c3e3
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 12:53:23 +0200

    React Native <> Added arrows next to head to change hair style

--- assets/components/customizeAvatar.tsx
+++ assets/components/customizeAvatar.tsx
@@ -13,6 +13,7 @@ import WomanShirt from "../images/character/womanShirt";
 import Eyes from "../images/character/Eyes";
 import MaleEyes from "../images/character/maleEyes";
 import FemaleEyes from "../images/character/femaleEyes";
+import Icon from "react-native-vector-icons/Ionicons";
 
 type AvatarProps = {
   hairColor: string;
@@ -23,25 +24,49 @@ type AvatarProps = {
   gender: string;
 };
 
-const CustomizeAvatar = ({hairColor, eyeColor, skinColor, shirtColor, backgroundColor, gender}: AvatarProps) => {
+const CustomizeAvatar = ({
+  hairColor,
+  eyeColor,
+  skinColor,
+  shirtColor,
+  backgroundColor,
+  gender,
+}: AvatarProps) => {
   return (
-  <View style={[tw`flex flex-col items-center justify-end w-full`, { backgroundColor: backgroundColor }]}>
-    {gender === 'female' ? (
-      <>
-        <WomanHairThree hairColor={hairColor} style="relative top-17 z-2" />
-        <Eyes eyeColor={eyeColor} style="relative top-14.5 z-1" />
-        <WomanFace skinColor={skinColor} />
-        <WomanShirt shirtColor={shirtColor} />
-      </>
-    ) : (
-      <>
-        <ManHairThree hairColor={hairColor} style="relative top-17 z-2" />
-        <Eyes eyeColor={eyeColor} style="relative top-14.5 z-1" />
-        <MaleFace skinColor={skinColor} />
-        <ManShirt shirtColor={shirtColor} />
-      </>
-    )}
-  </View>
+    <View
+      style={[
+        tw`flex flex-col items-center justify-end w-full`,
+        { backgroundColor: backgroundColor },
+      ]}
+    >
+      {gender === "female" ? (
+        <>
+          <WomanHairThree hairColor={hairColor} style="relative top-17 z-2" />
+          <View
+            style={tw`flex top-15 flex-row justify-between items-center w-1/2`}
+          >
+            <Icon name="arrow-back-outline" size={36} />
+            <Icon name="arrow-forward-outline" size={36} />
+          </View>
+          <Eyes eyeColor={eyeColor} style="relative top-14.5 z-1" />
+          <WomanFace skinColor={skinColor} />
+          <WomanShirt shirtColor={shirtColor} />
+        </>
+      ) : (
+        <>
+          <ManHairThree hairColor={hairColor} style="relative top-17 z-2" />
+          <View
+            style={tw`top-15 flex flex-row justify-between items-center w-1/2`}
+          >
+            <Icon name="arrow-back-outline" size={36} />
+            <Icon name="arrow-forward-outline" size={36} />
+          </View>
+          <Eyes eyeColor={eyeColor} style="relative top-14.5 z-1" />
+          <MaleFace skinColor={skinColor} />
+          <ManShirt shirtColor={shirtColor} />
+        </>
+      )}
+    </View>
   );
 };
 
