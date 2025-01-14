d43ce1c361205b10ef7994a1c201b18f0c90e6db
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 15:51:54 +0200

    React Native <> Added alert modal to level 1

--- app/levels/Level1.tsx
+++ app/levels/Level1.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   SafeAreaView,
   Image,
@@ -10,13 +10,16 @@ import { useTheme } from "@/assets/utils/useTheme";
 import Back from "@/assets/components/Back";
 import Icon from "react-native-vector-icons/Ionicons";
 import tw from "twrnc";
+import AlertModal from "@/assets/components/alertModal";
 
 const Level1 = () => {
   const { themeViewStyle } = useTheme();
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <SafeAreaView style={tw`${themeViewStyle}flex-1 items-center`}>
-      <Back text="Level 1" background/>
+      <Back text="Level 1" background setModalVisible={setModalVisible}/>
+      <AlertModal modalVisible={modalVisible} setModalVisible={setModalVisible}/>
       <View style={tw`w-full h-3/4 items-center justify-start`}>
         <Image
           source={require("@/assets/images/game/coffeeshop-night-1.png")}
