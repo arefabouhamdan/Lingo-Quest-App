2d3d09d5b6cf1041b4f9d6cd9f65cf1334f9f87c
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 12:01:50 +0200

    React Native <> Added skin color options for avatar customization

---
+++ assets/utils/skinColors.ts
@@ -0,0 +1,32 @@
+export const SkinColors = [
+    {
+        "id": "1",
+        "color": "#F5CBA7",
+        "name": "Fair"
+    },
+    {
+        "id": "2",
+        "color": "#D2B48C",
+        "name": "Light"
+    },
+    {
+        "id": "3",
+        "color": "#C68642",
+        "name": "Medium"
+    },
+    {
+        "id": "4",
+        "color": "#8D5524",
+        "name": "Tan"
+    },
+    {
+        "id": "5",
+        "color": "#6B4226",
+        "name": "Dark"
+    },
+    {
+        "id": "6",
+        "color": "#4B2E15",
+        "name": "Deep"
+    }
+]
