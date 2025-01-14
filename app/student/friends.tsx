5d3a85f0d0c56fd69ce06a715dfff6d6e76269ab
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-15 01:42:14 +0200

    React Native <> Refactored friends component to display users's friends

--- app/student/friends.tsx
+++ app/student/friends.tsx
@@ -5,31 +5,12 @@ import { FlatList, SafeAreaView, View, Text } from "react-native";
 import { useTheme } from "@/hooks/useTheme";
 import tw from "twrnc";
 import Back from "@/assets/components/Back";
+import { useStorage } from "@/hooks/useStorage";
 
 const Friends = () => {
   const { themeViewStyle, themeTextStyle } = useTheme();
-  const user = {
-    name: "Aref",
-    id: "1234",
-    joined: "01-09-2021",
-    streak: "20",
-    xp: "12000",
-    type: "student",
-    avatar: {
-      gender: "male",
-      background: { color: "#4FC0E8" },
-      hair: { color: "#674238", style: "ManHairOne" },
-      skin: { color: "#E7BC98", style: "MaleFace" },
-      eyes: { color: "#1E81C8", style: "Eyes" },
-      shirt: { color: "#484848", style: "ManShirt" },
-    },
-    friends: [
-      { name: "Aref", dateAdded: "01-09-2021" },
-      { name: "Bilal", dateAdded: "01-09-2021" },
-      { name: "Hassan", dateAdded: "01-09-2021" },
-      { name: "Ali", dateAdded: "01-09-2021" },
-    ],
-  };
+  const { user } = useStorage();
+
   return (
     <SafeAreaView style={tw`${themeViewStyle} flex-1 items-center`}>
       <Back text="Friends" />
@@ -37,19 +18,17 @@ const Friends = () => {
         <Search text="Lost your friend? Search for him" />
       </View>
       <FlatList
-        data={user.friends}
-        renderItem={({ item }) => (
-          <User name={item.name} />
-        )}
-        ListEmptyComponent={() => (
-          <View style={tw`flex-1 items-center justify-center`}>
-            <Text style={tw`${themeTextStyle} text-xl font-bold`}>
-              You have no friends? Sad
-            </Text>
-          </View>
-        )}
+        data={user?.friends}
+        renderItem={({ item }) => <User name={item.name} />}
         style={tw`mt-5 w-11/12`}
       />
+      {user?.friends == 0 && (
+        <View style={tw`absolute top-1/2`}>
+          <Text style={tw`${themeTextStyle} text-xl font-bold`}>
+            You have no friends? Sad
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
