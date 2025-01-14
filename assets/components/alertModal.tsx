213c1f2889579a5b3a652df09aff7dc5b7f294e8
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 15:59:40 +0200

    React Native <> Added "are you sure" text to alert Modal

--- assets/components/alertModal.tsx
+++ assets/components/alertModal.tsx
@@ -10,14 +10,13 @@ type FriendModalProps = {
   setModalVisible: (modalVisible: boolean) => void;
 };
 
-
 const AlertModal = ({ modalVisible, setModalVisible }: FriendModalProps) => {
   const { themeViewStyle, themeTextStyle } = useTheme();
   const navigation = useNavigation();
   const handleLeave = () => {
-    setModalVisible(!modalVisible)
-    navigation.goBack()
-  }
+    setModalVisible(!modalVisible);
+    navigation.goBack();
+  };
   return (
     <Modal
       animationType="slide"
@@ -29,21 +28,25 @@ const AlertModal = ({ modalVisible, setModalVisible }: FriendModalProps) => {
         style={tw`flex-1 justify-center items-center bg-black bg-opacity-50`}
       >
         <View
-          style={tw`${themeViewStyle} border-4 border-white rounded-2 w-92 h-130 flex flex-col items-center`}
+          style={tw`${themeViewStyle} border-4 border-white rounded-2 w-92 h-130 flex flex-col items-center justify-center`}
         >
-         
           <TouchableOpacity
             style={tw`absolute top-5 right-5`}
             onPress={() => setModalVisible(!modalVisible)}
           >
             <Icon name="close-outline" color="white" size={36} />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={tw`w-52 bg-sky-400 rounded-2 h-14 my-auto flex flex-row justify-center items-center gap-5`}
-            onPress={handleLeave}
-          >
-            <Text style={tw`text-white font-bold text-lg`}>Leave</Text>
-          </TouchableOpacity>
+          <View style={tw`flex flex-col items-center justify-center gap-10`}>
+            <Text style={tw`${themeTextStyle} text-xl font-bold `}>
+              Are you sure you want to leave?
+            </Text>
+            <TouchableOpacity
+              style={tw`w-52 bg-sky-400 rounded-2 h-14 my-auto flex flex-row justify-center items-center gap-5`}
+              onPress={handleLeave}
+            >
+              <Text style={tw`text-white font-bold text-lg`}>Leave</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </Modal>
