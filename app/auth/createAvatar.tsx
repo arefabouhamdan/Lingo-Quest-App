04344315b0b99bdd2f132b71b09fa3edb41c5b64
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 11:48:31 +0200

    React Native <> Refactored CreateAvatar component to manage avatar customization state

--- app/auth/createAvatar.tsx
+++ app/auth/createAvatar.tsx
@@ -1,32 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   View,
   TouchableOpacity,
   Text,
-  FlatList,
 } from "react-native";
 import { useTheme } from "@/assets/utils/useTheme";
 import tw from "twrnc";
-import Avatar from "@/assets/components/Avatar";
 import Tabs from "@/assets/components/tabs";
+import CustomizeAvatar from "@/assets/components/customizeAvatar";
 
 const CreateAvatar = () => {
+  const [hairColor, setHairColor] = useState("blue");
+  const [eyeColor, setEyeColor] = useState("red");
+  const [skinColor, setSkinColor] = useState("pink");
+  const [shirtColor, setShirtColor] = useState("green");
+  const [backgroundColor, setBackgroundColor] = useState("gray");
+  const [gender, setGender] = useState("male");
+  
   const { themeViewStyle } = useTheme();
   const buttonStyle =
     "flex justify-center items-center p-1 w-9/20 h-14 rounded-2 border-r-4 border-b-4";
   const fontStyle = "font-bold text-lg py-2 text-white";
-  const user = {
-    type: "student",
-    avatar: {
-      gender: "male",
-      background: { color: "#4FC0E8" },
-      hair: { color: "#674238", style: "ManHairOne" },
-      skin: { color: "#E7BC98", style: "MaleFace" },
-      eyes: { color: "#1E81C8", style: "Eyes" },
-      shirt: { color: "#484848", style: "ManShirt" },
-    },
-  };
   return (
     <SafeAreaView style={tw`${themeViewStyle} flex-1`}>
       <View style={tw`flex flex-row justify-between items-center px-5 my-5`}>
@@ -39,9 +34,16 @@ const CreateAvatar = () => {
           <Text style={tw`${fontStyle}`}>Female</Text>
         </TouchableOpacity>
       </View>
-      <Avatar user={user} />
+      <CustomizeAvatar
+        hairColor={hairColor}
+        eyeColor={eyeColor}
+        skinColor={skinColor}
+        backgroundColor={backgroundColor}
+        shirtColor={shirtColor}
+        gender={gender}
+      />
       <View style={tw`px-5 my-5`}>
-        <Tabs />
+        <Tabs setHairColor={setHairColor}/>
       </View>
     </SafeAreaView>
   );
