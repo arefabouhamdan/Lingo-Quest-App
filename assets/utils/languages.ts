d6c37fdf916be88cb0cbe9cf77f78e524280a388
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 13:59:12 +0200

    React Native <> Added language options

---
+++ assets/utils/languages.ts
@@ -0,0 +1,32 @@
+export const languages = [
+    {
+        id: 1,
+        name: "English",
+        code: "en",
+    },
+    {
+        id: 2,
+        name: "Spanish",
+        code: "es",
+    },
+    {
+        id: 3,
+        name: "French",
+        code: "fr",
+    },
+    {
+        id: 4,
+        name: "German",
+        code: "de",
+    },
+    {
+        id: 5,
+        name: "Italian",
+        code: "it",
+    },
+    {
+        id: 6,
+        name: "Arabic",
+        code: "ar",
+    }
+]
