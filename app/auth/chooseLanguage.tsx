1764d37bf50287b378588670eb721a84a90e839a
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-14 14:22:37 +0200

    React Native <> Added Languages FlatList to the chooseLanguage screen

--- app/auth/chooseLanguage.tsx
+++ app/auth/chooseLanguage.tsx
@@ -1,9 +1,10 @@
-import { Text, Image, View } from "react-native";
+import { Text, Image, View, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTheme } from "../../assets/utils/useTheme";
 import tw from "twrnc";
 import Button from "../../assets/components/Button";
 import Back from "@/assets/components/Back";
+import { languages } from "../../assets/utils/languages";
 
 const ChooseLanguage = () => {
   const { themeTextStyle, themeViewStyle } = useTheme();
@@ -12,13 +13,16 @@ const ChooseLanguage = () => {
     <SafeAreaView
       style={tw`${themeViewStyle} flex-1 items-center justify-center`}
     >
-      <Back/>
+      <Back />
       <View style={tw`flex flex-col items-center my-auto gap-20`}>
         <Image
           source={require("../../assets/images/game/guide-choose.png")}
           style={tw`w-52 h-68 mb-5`}
         />
-        
+        <FlatList
+          data={languages}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+        />
       </View>
     </SafeAreaView>
   );
