ca2b8482fafd25d8a07dfa8e47ad115e0be1e29b
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-15 16:42:46 +0200

    Tailwind <> Updated leaderboard colors and margins in rank component

--- assets/components/rank.tsx
+++ assets/components/rank.tsx
@@ -15,10 +15,10 @@ const Rank = ({data}: RankProps) => {
   return (
     <View style={tw`mt-10`}>
       <View style={tw`flex flex-row items-end justify-center gap-3.5`}>
-        <View style={tw`flex flex-col items-center justify-center gap-1`}>
+        <View style={tw`flex flex-col items-center justify-center gap-1 mr-5`}>
           {/* <Avatar user={data[1]} /> */}
           <Text style={tw`${themeTextStyle} text-lg`}>{data[1]?.name}</Text>
-          <View style={tw`bg-sky-400 w-11 h-24 rounded-t`}></View>
+          <View style={tw`bg-gray-300 w-11 h-26 rounded-t`}></View>
         </View>
         <View style={tw`flex flex-col items-center justify-center gap-1`}>
           <Image
@@ -26,15 +26,15 @@ const Rank = ({data}: RankProps) => {
             style={tw`w-9 h-14`}
           />
           <Text style={tw`${themeTextStyle} text-lg`}>{data[0]?.name}</Text>
-          <View style={tw`bg-sky-400 w-11 h-36 rounded-t`}></View>
+          <View style={tw`bg-yellow-500 w-11 h-36 rounded-t`}></View>
         </View>
-        <View style={tw`flex flex-col items-center justify-center gap-1`}>
+        <View style={tw`flex flex-col items-center justify-center gap-1 ml-5`}>
           <Image
             source={require(`@/assets/images/character/Aref.png`)}
             style={tw`w-9 h-14`}
           />
           <Text style={tw`${themeTextStyle} text-lg`}>{data[2]?.name}</Text>
-          <View style={tw`bg-sky-400 w-11 h-20 rounded-t`}></View>
+          <View style={tw`bg-amber-700 w-11 h-18 rounded-t`}></View>
         </View>
       </View>
     </View>
