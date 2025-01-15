8f7a3c1af39c802514f7ffab88e4d95658008e86
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-15 15:17:18 +0200

    React Native <> Updated LeaderList component to accept data as props

--- assets/components/leaderList.tsx
+++ assets/components/leaderList.tsx
@@ -1,31 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { FlatList, View, Text } from "react-native";
-import { useQuery } from "react-query";
 import { useTheme } from "@/hooks/useTheme";
-import axios from "axios";
 import User from "./User";
 import tw from "twrnc";
 
-const LeaderList = () => {
-  const fetchUsers = async () => {
-    const response = await axios.get(`http://192.168.1.102:3000/users`);
-    return response.data;
-  };
+type LeaderListProps = {
+  data: any[];
+  isLoading: boolean;
+}
 
-  const { data, isLoading, refetch } = useQuery(
-    ["userLeaderboard"],
-    () => fetchUsers(),
-    {
-      enabled: false,
-    }
-  );
-
-  useEffect(() => {
-    refetch();
-  }, []);
-
+const LeaderList= ({ data, isLoading } : LeaderListProps) => {
   const { themeTextStyle } = useTheme();
-
   return (
     <View style={tw`w-11/12 flex-1 border border-gray-200 rounded-lg `}>
       {!isLoading ? (
@@ -38,7 +23,9 @@ const LeaderList = () => {
         />
       ) : (
         <View style={tw`flex-1 items-center justify-center`}>
-          <Text style={tw`${themeTextStyle} text-lg font-bold`}>Loading...</Text>
+          <Text style={tw`${themeTextStyle} text-lg font-bold`}>
+            Loading...
+          </Text>
         </View>
       )}
     </View>
