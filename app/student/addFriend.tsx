78a4b4e8c4abf61ed3d57304f67fa79b62a22d99
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-15 11:27:12 +0200

    React Native <> Linked users list with addFriends component to search for users

--- app/student/addFriend.tsx
+++ app/student/addFriend.tsx
@@ -1,40 +1,73 @@
-import { View, Text, SafeAreaView, FlatList } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useTheme } from '@/hooks/useTheme'
-import Back from '@/assets/components/Back'
-import Search from '@/assets/components/Search'
-import { useStorage } from '@/hooks/useStorage'
-import User from '@/assets/components/User'
-import tw from 'twrnc'
+import { View, Text, SafeAreaView, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "@/hooks/useTheme";
+import Back from "@/assets/components/Back";
+import Search from "@/assets/components/Search";
+import { useQuery } from "react-query";
+import axios from "axios";
+import User from "@/assets/components/User";
+import { useStorage } from "@/hooks/useStorage";
+import tw from "twrnc";
 
 const AddFriend = () => {
-  const { user } = useStorage();
   const [search, setSearch] = useState("");
   const { themeViewStyle, themeTextStyle } = useTheme();
+  const { user } = useStorage();
+
+  const fetchUsers = async () => {
+    const response = await axios.get(`http://192.168.1.102:3000/users`);
+    return response.data;
+  };
+
+  const {
+    data,
+    isLoading,
+    refetch,
+  } = useQuery(["user", search], () => fetchUsers(), {
+    enabled: false,
+  });
+
   useEffect(() => {
-    console.log(search);
-  }, [search]);
+    if (search.trim()) {
+      refetch();
+    }
+  }, [search, refetch]);
+
+  const users = data?.filter((input) => input.name.includes(search)).filter((input) => input.name !== user.name);
 
   return (
     <SafeAreaView style={tw`${themeViewStyle} flex-1 items-center`}>
-       <Back text="Add Friend" />
+      <Back text="Add Friend" />
       <View style={tw`mt-5 w-11/12`}>
-        <Search text="Enter name" setSearch={setSearch}/>
+        <Search text="Enter name" setSearch={setSearch} />
       </View>
+      {isLoading && (
+        <Text style={tw`${themeTextStyle} mt-5 text-xl font-bold`}>
+          Searching...
+        </Text>
+      )}
       <FlatList
-        data={user?.friends}
-        renderItem={({ item }) => <User name={item} />}
+        data={users || []}
+        renderItem={({ item }) => <User name={item.name || item} />}
+        keyExtractor={(item, index) => item.id?.toString() || index.toString()}
         style={tw`mt-5 w-11/12`}
       />
-      {user?.friends == 0 && (
+      {!users && (
         <View style={tw`absolute top-1/2`}>
           <Text style={tw`${themeTextStyle} text-xl font-bold`}>
             Searching for a friend? Me too
           </Text>
         </View>
       )}
+      {!users?.length && !isLoading && search.trim() && (
+        <View style={tw`absolute top-1/2`}>
+          <Text style={tw`${themeTextStyle} text-xl font-bold`}>
+            No users found.
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default AddFriend
+export default AddFriend;
