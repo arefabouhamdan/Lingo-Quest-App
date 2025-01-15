e256eb2e57e3bc63de03c14b31fe1785ba1b3559
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-16 00:29:15 +0200

    React Native <> Added Question section to test screen

--- app/levels/test1.tsx
+++ app/levels/test1.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import React from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
 import { useTheme } from "@/hooks/useTheme";
 import tw from "twrnc";
 import Back from "@/assets/components/Back";
@@ -10,9 +10,14 @@ const Test1 = () => {
   const { themeViewStyle, themeTextStyle } = useTheme();
   return (
     <SafeAreaView style={tw`${themeViewStyle} flex-1 items-center`}>
-      <Back text="Test 1" />
-      <View style={tw`mt-5 w-11/12`}>
-        <Text>Test</Text>
+      <Back text="Test 1" background />
+      <View style={tw`w-full h-2/5 bg-sky-400 items-center justify-center gap-5`}>
+        <Text style={tw`${themeTextStyle} text-3xl font-bold text-white px-3`}>
+          Question 1:
+        </Text>
+        <Text style={tw`${themeTextStyle} text-3xl font-medium text-white px-3`}>
+          What is the capital of France?
+        </Text>
       </View>
     </SafeAreaView>
   );
