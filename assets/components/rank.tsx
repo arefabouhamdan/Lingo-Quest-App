908f23c063d82346e910f6ce8bb60c447a5e9996
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-15 22:11:29 +0200

    React Native <> Changed Rank component View to touchable opacity

--- assets/components/rank.tsx
+++ assets/components/rank.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { useTheme } from "@/hooks/useTheme";
 import tw from "twrnc";
 import Avatar from "./Avatar";
@@ -17,21 +17,21 @@ const Rank = ({data}: RankProps) => {
   return (
     <View style={tw`mt-10`}>
       <View style={tw`flex flex-row items-end justify-center gap-3.5`}>
-        <View style={tw`flex flex-col items-center justify-center gap-1 mr-5`}>
+        <TouchableOpacity style={tw`flex flex-col items-center justify-center gap-1 mr-5`}>
           <MiniAvatar user={data[1]} />
           <Text style={tw`${themeTextStyle} text-lg`}>{data[1]?.name}</Text>
           <View style={tw`bg-gray-300 w-11 h-26 rounded-t`}></View>
-        </View>
-        <View style={tw`flex flex-col items-center justify-center gap-1`}>
+        </TouchableOpacity>
+        <TouchableOpacity style={tw`flex flex-col items-center justify-center gap-1`}>
         <MiniAvatar user={data[0]} />
           <Text style={tw`${themeTextStyle} text-lg`}>{data[0]?.name}</Text>
           <View style={tw`bg-yellow-500 w-11 h-36 rounded-t`}></View>
-        </View>
-        <View style={tw`flex flex-col items-center justify-center gap-1 ml-5`}>
+        </TouchableOpacity>
+        <TouchableOpacity style={tw`flex flex-col items-center justify-center gap-1 ml-5`} >
         <MiniAvatar user={data[2]} />
           <Text style={tw`${themeTextStyle} text-lg`}>{data[2]?.name}</Text>
           <View style={tw`bg-amber-700 w-11 h-18 rounded-t`}></View>
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
