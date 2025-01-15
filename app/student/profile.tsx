2a8b6709a376e4bab4894cc085e59676b5025b05
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-15 15:39:04 +0200

    React Native <> Removed Id from profile and fixed stats alignment and font size

--- app/student/profile.tsx
+++ app/student/profile.tsx
@@ -23,10 +23,9 @@ const Profile = () => {
   const handleLogout = () => {
     logout();
     navigation.navigate("Welcome" as never);
-  }
+  };
 
   const { user } = useStorage();
-
 
   return (
     <SafeAreaView style={tw`${themeViewStyle} flex-1`}>
@@ -34,9 +33,9 @@ const Profile = () => {
         style={tw`absolute z-10 top-2 right-2 bg-red-700 px-5 py-3 rounded flex items-center justify-center`}
         onPress={handleLogout}
       >
-        <Icons name="log-out-sharp" size={36} color={"#fff"}/>
+        <Icons name="log-out-sharp" size={36} color={"#fff"} />
       </TouchableOpacity>
-      <Avatar user={user}/>
+      <Avatar user={user} />
       <View
         style={tw`flex flex-row px-3 justify-between items-center w-full mt-5`}
       >
@@ -45,7 +44,9 @@ const Profile = () => {
         </Text>
         <Image
           style={tw`h-4.4 w-6`}
-          source={languages.find((lang) => lang.name === user?.language)?.source}
+          source={
+            languages.find((lang) => lang.name === user?.language)?.source
+          }
         />
       </View>
       <View style={tw`flex flex-row justify-between items-center w-full`}>
@@ -53,10 +54,12 @@ const Profile = () => {
           <View
             style={tw`flex flex-row items-center mt-5 w-full justify-between`}
           >
-            <Text style={tw`${themeTextStyle} text-lg font-medium mt-2`}>
-              ID {user?._id}
+            <Text style={tw`${themeTextStyle} text-lg font-medium`}>
+              Joined {user?.createdAt.split("T")[0]}
             </Text>
-            <TouchableOpacity onPress={() => navigation.navigate("Friends" as never)}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Friends" as never)}
+            >
               <Text style={tw`${themeTextStyle} text-lg font-medium `}>
                 No. Friends{"  "}
                 <Text style={tw`text-sky-400 font-extrabold`}>
@@ -65,9 +68,6 @@ const Profile = () => {
               </Text>
             </TouchableOpacity>
           </View>
-          <Text style={tw`${themeTextStyle} text-lg font-medium`}>
-            Joined {user?.createdAt.split("T")[0]}
-          </Text>
         </View>
       </View>
       <View style={tw`flex items-center my-5`}>
@@ -79,33 +79,39 @@ const Profile = () => {
         />
       </View>
 
-      <Text style={tw`${themeTextStyle} text-xl font-bold mb-2 text-left pl-3`}>
-        Stats
-      </Text>
-      <View style={tw`flex flex-row gap-2 items-center px-2 justify-center mb-5`}>
+      <View style={tw`my-auto flex gap-5`}>
+        <Text style={tw`${themeTextStyle} text-2xl font-bold text-left pl-3`}>
+          Stats
+        </Text>
         <View
-          style={tw`flex items-center w-1/2.1 gap-2 border-2 border-b-4 rounded-2 border-gray-300`}
+          style={tw`flex flex-row gap-2 items-center px-2 justify-center mb-5`}
         >
-          <Text style={tw`${themeTextStyle} text-xl my-2 font-medium`}>
-            Day Streak
-          </Text>
-          <Image
-            source={require("@/assets/images/icons/fire.png")}
-            style={tw`h-26 w-22`}
-          />
-          <Text style={tw`${themeTextStyle} text-xl my-2`}>{user?.streak} days</Text>
-        </View>
-        <View
-          style={tw`flex items-center w-1/2.1 gap-2 border-2 border-b-4 rounded-2 border-gray-300`}
-        >
-          <Text style={tw`${themeTextStyle} text-xl my-2 font-medium`}>
-            Total XP
-          </Text>
-          <Image
-            source={require("@/assets/images/icons/bolt.png")}
-            style={tw`h-26 w-19`}
-          />
-          <Text style={tw`${themeTextStyle} text-xl my-2`}>{user?.xp} XP</Text>
+          <View
+            style={tw`flex items-center w-1/2.1 gap-2 border-2 border-b-4 rounded-2 border-gray-300`}
+          >
+            <Text style={tw`${themeTextStyle} text-xl my-2 font-medium`}>
+              Days Played
+            </Text>
+            <Image
+              source={require("@/assets/images/icons/fire.png")}
+              style={tw`h-26 w-22`}
+            />
+            <Text style={tw`${themeTextStyle} text-xl my-2`}>
+              {user?.streak} days
+            </Text>
+          </View>
+          <View
+            style={tw`flex items-center w-1/2.1 gap-2 border-2 border-b-4 rounded-2 border-gray-300`}
+          >
+            <Text style={tw`${themeTextStyle} text-xl my-2 font-medium`}>
+              Total XP
+            </Text>
+            <Image
+              source={require("@/assets/images/icons/bolt.png")}
+              style={tw`h-26 w-19`}
+            />
+            <Text style={tw`${themeTextStyle} text-xl my-2`}>{user?.xp} XP</Text>
+          </View>
         </View>
       </View>
     </SafeAreaView>
