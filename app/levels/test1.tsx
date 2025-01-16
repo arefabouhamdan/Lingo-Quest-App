b9411d7fc5872346e202c352f221ce0d468516fc
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-16 06:13:26 +0200

    Tailwind <> Refactored button styles in Test1 component for improved readability

--- app/levels/test1.tsx
+++ app/levels/test1.tsx
@@ -8,10 +8,19 @@ import { useStorage } from "@/hooks/useStorage";
 
 const Test1 = () => {
   const { themeViewStyle, themeTextStyle } = useTheme();
+  const prevStyle = 0 ? "opacity-50" : "opacity-100";
+  const nextStyle =
+    1
+      ? "opacity-50"
+      : "opacity-100";
+  const buttonStyle =
+    "w-23 h-11 bg-sky-400 flex justify-center items-center rounded-2 my-auto border-sky-600";
+  const textStyle = "text-white text-xl font-medium";
+  const answerStyle = "w-11/12 h-1/15 mt-5 bg-sky-400 justify-center rounded px-3";
   return (
     <SafeAreaView style={tw`${themeViewStyle} flex-1 items-center`}>
       <Back text="Test 1" background />
-      <View style={tw`w-full h-2/5 bg-sky-400 items-center justify-center gap-5`}>
+      <View style={tw`w-full h-2/5 bg-sky-400 items-start justify-center gap-5`}>
         <Text style={tw`${themeTextStyle} text-3xl font-bold text-white px-3`}>
           Question 1:
         </Text>
@@ -19,24 +28,24 @@ const Test1 = () => {
           What is the capital of France?
         </Text>
       </View>
-      <TouchableOpacity style={tw`w-11/12 h-1/15 mt-5 bg-sky-400 justify-center rounded px-3`}>
-        <Text style={tw`text-white font-bold text-xl`}>Answer 1</Text>
+      <TouchableOpacity style={tw`${answerStyle}`}>
+        <Text style={tw`${textStyle}`}>Answer 1</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={tw`w-11/12 h-1/15 mt-5 bg-sky-400 justify-center rounded px-3`}>
-        <Text style={tw`text-white font-bold text-xl`}>Answer 2</Text>
+      <TouchableOpacity style={tw`${answerStyle}`}>
+        <Text style={tw`${textStyle}`}>Answer 2</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={tw`w-11/12 h-1/15 mt-5 bg-sky-400 justify-center rounded px-3`}>
-        <Text style={tw`text-white font-bold text-xl`}>Answer 3</Text>
+      <TouchableOpacity style={tw`${answerStyle}`}>
+        <Text style={tw`${textStyle}`}>Answer 3</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={tw`w-11/12 h-1/15 mt-5 bg-sky-400 justify-center rounded px-3`}>
-        <Text style={tw`text-white font-bold text-xl`}>Answer 4</Text>
+      <TouchableOpacity style={tw`${answerStyle}`}>
+        <Text style={tw`${textStyle}`}>Answer 4</Text>
       </TouchableOpacity>
-      <View style={tw`h-1/15 flex-row mt-5 px-3 gap-5`}>
-        <TouchableOpacity style={tw`w-1/3 h-full mt-5 bg-sky-400 justify-center rounded px-3`}>
-          <Text style={tw`text-white font-bold text-xl text-center`}>Prev</Text>
+      <View style={tw`h-1/15 flex-row my-auto px-3 gap-5`}>
+        <TouchableOpacity style={tw`${buttonStyle} ${prevStyle}`}>
+          <Text style={tw`${textStyle}`}>Prev</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={tw`w-1/3 h-full mt-5 bg-sky-400 justify-center rounded px-3`}>
-          <Text style={tw`text-white font-bold text-xl text-center`}>Next</Text>
+        <TouchableOpacity style={tw`${buttonStyle} ${nextStyle}`}>
+          <Text style={tw`${textStyle}`}>Next</Text>
         </TouchableOpacity>
       </View>
       
