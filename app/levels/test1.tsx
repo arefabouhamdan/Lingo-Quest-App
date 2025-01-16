cbf4a17ce01a2fa58a4868824691b75fc8d3c11f
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-16 07:30:47 +0200

    React Native <> Implemented navigation for question data in Test1 component with previous and next buttons

--- app/levels/test1.tsx
+++ app/levels/test1.tsx
@@ -25,12 +25,21 @@ const Test1 = () => {
       retry: false,
     }
   );
-  console.log(question?.data[0].content);
   
 
+  const [index, setIndex] = useState(0);
+  console.log("index", index)
+  
+  const handleIncrement = () => {
+    index <= question?.data.length - 1 ? setIndex(index + 1): null;
+  }
+  const handleDecrement = () => {
+    index == 0 ? null : setIndex(index - 1);
+  }
+
   const { themeViewStyle, themeTextStyle } = useTheme();
-  const prevStyle = 0 ? "opacity-50" : "opacity-100";
-  const nextStyle = 1 ? "opacity-50" : "opacity-100";
+  const prevStyle = index == 0 ? "opacity-50" : "opacity-100";
+  const nextStyle = index < question?.data.length - 1 ? "opacity-100" : "opacity-50";
   const buttonStyle =
     "w-23 h-11 bg-sky-400 flex justify-center items-center rounded-2 my-auto border-sky-600";
   const textStyle = "text-white text-xl font-medium";
@@ -53,26 +62,26 @@ const Test1 = () => {
         <Text
           style={tw`${themeTextStyle} text-3xl font-medium text-white px-3`}
         >
-          {question?.data[0].content}
+          {question?.data[index].content}
         </Text>
       </View>
       <TouchableOpacity style={tw`${answerStyle}`}>
-        <Text style={tw`${textStyle}`}>{question?.data[0].choices[0]}</Text>
+        <Text style={tw`${textStyle}`}>{question?.data[index].choices[0]}</Text>
       </TouchableOpacity>
       <TouchableOpacity style={tw`${answerStyle}`}>
-        <Text style={tw`${textStyle}`}>{question?.data[0].choices[1]}</Text>
+        <Text style={tw`${textStyle}`}>{question?.data[index].choices[1]}</Text>
       </TouchableOpacity>
       <TouchableOpacity style={tw`${answerStyle}`}>
-        <Text style={tw`${textStyle}`}>{question?.data[0].choices[2]}</Text>
+        <Text style={tw`${textStyle}`}>{question?.data[index].choices[2]}</Text>
       </TouchableOpacity>
       <TouchableOpacity style={tw`${answerStyle}`}>
-        <Text style={tw`${textStyle}`}>{question?.data[0].choices[3]}</Text>
+        <Text style={tw`${textStyle}`}>{question?.data[index].choices[3]}</Text>
       </TouchableOpacity>
       <View style={tw`h-1/15 flex-row my-auto px-3 gap-5`}>
-        <TouchableOpacity style={tw`${buttonStyle} ${prevStyle}`}>
+        <TouchableOpacity style={tw`${buttonStyle} ${prevStyle}`} onPress={handleDecrement} disabled={index == 0}>
           <Text style={tw`${textStyle}`}>Prev</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={tw`${buttonStyle} ${nextStyle}`}>
+        <TouchableOpacity style={tw`${buttonStyle} ${nextStyle}`} onPress={handleIncrement} disabled={index == question?.data.length - 1}>
           <Text style={tw`${textStyle}`}>Next</Text>
         </TouchableOpacity>
       </View>
