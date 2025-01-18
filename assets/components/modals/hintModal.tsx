1f16515d4bd310593e83e1cc74bdc2d0df7f9c65
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-18 19:43:04 +0200

    React Native <> Added close button in hintModal

--- assets/components/modals/hintModal.tsx
+++ assets/components/modals/hintModal.tsx
@@ -3,6 +3,7 @@ import { Modal, Text, TouchableOpacity, View, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useTheme } from "../../../hooks/useTheme";
 import Penguin from "@/assets/images/game/guide-tutor";
+import Icon from "react-native-vector-icons/Ionicons";
 import tw from "twrnc";
 
 type FriendModalProps = {
@@ -27,6 +28,9 @@ const AlertModal = ({ modalVisible, setModalVisible }: FriendModalProps) => {
       <View
         style={tw`flex-1 justify-center items-center bg-black bg-opacity-50`}
       >
+        <TouchableOpacity style={tw`absolute top-5 right-5`} onPress={(prev) => setModalVisible(!prev)}>
+            <Icon name="close-outline" size={36} color={"white"}/>
+        </TouchableOpacity>
         <View
           style={tw`${themeViewStyle} border-4 border-white rounded-5 w-85 h-50 flex flex-col absolute top-60`}
         >
