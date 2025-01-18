de3ded547f178066f82cd9ce0c6a703771ac2659
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-18 11:02:54 +0200

    React Native <> Added remaining prop types  and navigation

--- hooks/useRegistration.ts
+++ hooks/useRegistration.ts
@@ -1,30 +1,31 @@
-import { useMutation } from 'react-query';
-import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
-import { BASE_URL } from '@/assets/utils/baseUrl';
+import { useMutation } from "react-query";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+import { BASE_URL } from "@/assets/utils/baseUrl";
 
 interface RegisterData {
-  username: string;
+  name: string;
   email: string;
   password: string;
+  type: string;
+  avatar: string[];
+  language: string;
 }
 
 const register = async (credentials: RegisterData) => {
-  
-    const response = await axios.post(
-      `${BASE_URL}/users`,
-      credentials
-    );
-    return response.data;
-  };
+  const response = await axios.post(`${BASE_URL}/users`, credentials);
+  return response.data;
+};
 
 export const useRegister = () => {
+  const navigation = useNavigation();
   return useMutation(register, {
     onSuccess: (response) => {
-      console.log('Registration successful!', response.data);
+      console.log("Registration successful!", response.data);
+      {response.data.type === "user" ? navigation.navigate("MainHome" as never) : navigation.navigate("TutorHome" as never)}
     },
     onError: (error: any) => {
-      console.error('Registration failed:', error);
+      console.error("Registration failed:", error);
     },
   });
-}
+};
