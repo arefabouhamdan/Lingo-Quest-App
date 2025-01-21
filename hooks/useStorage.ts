12f6fb50c8b443ff23dd9a2add2430b15168afb7
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-21 22:54:34 +0200

    React Native <> Updated useStorage to remove token alongside the user in logout function

--- hooks/useStorage.ts
+++ hooks/useStorage.ts
@@ -24,6 +24,7 @@ export const useStorage = () => {
   const logout = async () => {
     try {
       await AsyncStorage.removeItem('user');
+      await AsyncStorage.removeItem('token');
       setUser(null);
     } catch (error) {
       console.error('Error during logout:', error);
