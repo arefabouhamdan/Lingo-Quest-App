e2413cff5e3ddc136b6c7f30082752a6e017111e
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-22 16:08:59 +0200

    React Native <> Changed asyncStorage to secure storage in useStorage hook

--- hooks/useStorage.ts
+++ hooks/useStorage.ts
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
 
 export const useStorage = () => {
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const storedUser = await AsyncStorage.getItem('user');
+        const storedUser = await SecureStore.getItemAsync('user');
+        const storedToken = await SecureStore.getItemAsync('token');
         setUser(storedUser ? JSON.parse(storedUser) : null);
+        setToken(storedToken ? JSON.parse(storedToken) : null);
       } catch (error) {
         console.error('Error fetching user data:', error);
       } finally {
@@ -23,13 +26,14 @@ export const useStorage = () => {
 
   const logout = async () => {
     try {
-      await AsyncStorage.removeItem('user');
-      await AsyncStorage.removeItem('token');
+      await SecureStore.deleteItemAsync('user');
+      await SecureStore.deleteItemAsync('token');
       setUser(null);
+      setToken(null)
     } catch (error) {
       console.error('Error during logout:', error);
     }
   };
 
-  return { user, loading, logout};
+  return { user,token, loading, logout};
 };
