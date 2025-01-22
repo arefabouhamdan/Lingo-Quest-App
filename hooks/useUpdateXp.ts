2ca128de5eed7433168f664805602ff17516f5d4
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-22 16:26:59 +0200

    React Native <> Changed asyncStorage to secure storage in useUpdateXP hook

--- hooks/useUpdateXp.ts
+++ hooks/useUpdateXp.ts
@@ -1,7 +1,7 @@
 import { useMutation } from "react-query";
 import axios from "axios";
 import { BASE_URL } from "@/assets/utils/baseUrl";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as SecureStore from "expo-secure-store";
 
 interface UpdateData {
   username: string;
@@ -19,7 +19,8 @@ export const useUpdate = () => {
   return useMutation(update, {
     onSuccess: async (response) => {
       console.log("Updated successful!", JSON.stringify(response));
-      await AsyncStorage.setItem("user", JSON.stringify(response));
+      await SecureStore.deleteItemAsync("user");
+      await SecureStore.setItemAsync("user", JSON.stringify(response));
     },
     onError: (error: any) => {
       console.error("Update failed:", error);
