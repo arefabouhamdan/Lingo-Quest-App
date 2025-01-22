644a3183fd2e4622dcf746343a1f7051a3b3388b
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-22 22:42:47 +0200

    React Native <> Added user refresh on level screen

--- app/student/levels.tsx
+++ app/student/levels.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback} from "react";
 import { SafeAreaView, FlatList } from "react-native";
 import { useTheme } from "@/hooks/useTheme";
 import Level from "@/assets/components/level";
@@ -6,10 +6,36 @@ import LanguageBar from "@/assets/components/languageBar";
 import tw from "twrnc";
 import { useStorage } from "@/hooks/useStorage";
 import { levelsData } from "@/assets/utils/levels";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { BASE_URL } from "@/assets/utils/baseUrl";
+import { useFocusEffect } from "@react-navigation/native";
 
 const Levels = () => {
   const { themeViewStyle } = useTheme();
   const { user } = useStorage();
+  const fetchUserByName = async (name: string) => {
+    const response = await axios.get(`${BASE_URL}/users/${name}`);
+    return response.data;
+  };
+  
+  
+  const { data, isLoading, refetch } = useQuery(
+    ["user", user?.name],
+    () => fetchUserByName(user?.name),
+    {
+      enabled: false,
+    }
+  );
+
+  useFocusEffect(
+      useCallback(() => {
+        const fetchData = async () => {
+          const { data } = await refetch();
+        };
+        fetchData();
+      }, [refetch])
+    );
 
   return (
     <SafeAreaView
@@ -21,7 +47,7 @@ const Levels = () => {
         data={levelsData}
         renderItem={({ item, index }) => (
           <Level
-            status={user?.level >= index + 1 ? "active" : "inactive"}
+            status={data?.level >= index + 1 ? "active" : "inactive"}
             level={item.level}
             type={item.type}
           />
